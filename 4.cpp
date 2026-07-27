b51afe66c89cab307db9ff1164c4aa0e40f123c0
Ayush raj singh <ayushyoraj@gmail.com>
2026-07-27 15:54:47 +0530

    new update

---
+++ 4.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+int number1 , number2;
+cout<<"enter the value number1: \n";
+cin>>number1;
+cout<<"enter the value number2: \n";
+cin>>number2;
+cout<<"the sum is  "<<number1+number2;
+return 0;
+}
