e85a3716cbd1eac3b47aff22f36e381fe72a2ba2
Ayush raj singh <ayushyoraj@gmail.com>
2026-07-27 14:18:37 +0530

    Learning C++

---
+++ 1.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+// i am leraning coding hope it works wall for me 
+// so i am testing coumment 
+// now milplil coumment  
+int main(){
+    int sum = 65;
+
+
+    std::cout<<"let hope it works"<<sum;
+    return 0;
+}  
+
+// comment and virbal
---
+++ 2.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+int glo = 63;
+void sum(){
+    int a; 
+    cout<<glo;
+ }
+
+int main(){
+    // int a= 8;
+    // int b= 5;
+    // int c= 5;
+    int a = 23 , b = 27 ,c = 56 ;
+    float d = 3.4;
+    char t = 'y';
+    bool is_true = false;
+
+    cout<<"hope it works. \n this is value of a "<<a << " this the value of b \n" <<b << " this the value of c\n "<<c ;
+    cout<<" this the value of d "<<d;
+    cout<<" this the value of t "<<t;
+    cout<<" this the value of is it true "<<is_true;
+    return 0;
+
+
+
+}
