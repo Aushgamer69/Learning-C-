f4aef4c295c99df33b623ee42b8089c79b445a8a
Ayush raj singh <ayushyoraj@gmail.com>
2026-07-28 14:39:13 +0530

    new update

---
+++ 5.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+// #include"5a.cpp"
+
+int main(){
+    int a=4,b=7;
+    cout<<"hi hope it works "<<endl;
+    // arithmetic operator
+    cout<<"the sum of a+b is "<<a+b<<endl;
+    cout<<"the sum of a-b is "<<a-b<<endl;
+    cout<<"the sum of a*b is "<<a*b<<endl;
+    cout<<"the sum of a/b is "<<a/b<<endl;
+    cout<<"the sum of a++b is "<<a++<<endl;
+    cout<<"the sum of a--b is "<<a--<<endl;
+    cout<<endl;
+
+    // comparison operator
+    cout<<"the sum of a==b is "<<(a==b)<<endl;
+    cout<<"the sum of a!=b is "<<(a!=b)<<endl;
+    cout<<"the sum of a>=b is "<<(a>=b)<<endl;
+    cout<<"the sum of a<=b is "<<(a<=b)<<endl;
+    cout<<"the sum of a>b is "<<(a<b)<<endl;
+    cout<<"the sum of a<b is "<<(a>b)<<endl;
+    cout<<endl;
+    return 0;
+     
+}
