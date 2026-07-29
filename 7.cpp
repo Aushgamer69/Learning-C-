5d93f45b89ffaf23d30a029f8b383f270dd46963
Ayush raj singh <ayushyoraj@gmail.com>
2026-07-29 14:15:59 +0530

    new update

---
+++ 7.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main(){
+    // int a = 56 ; 
+    // char c = 'c';
+    // cout<<"this the value of a : "<<a;
+    // a = 67;
+    // c = '5';
+
+    // cout<<"THIS THE value of a : "<<a;
+    // float a = 4.23;
+    // cout<<"this the value of a was : "<<a<<endl;
+    // a = 45.34;
+    // cout<<"this the value of a was : "<<a<<endl;
+    int a = 34, b= 65;
+    int c = ((((a*6)+7)-12)+76);
+    cout<<c;
+
+
+    return  0;
+}
