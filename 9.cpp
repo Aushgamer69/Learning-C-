82d2e01b1e9884e475561fe369d81172a4757709
Ayush raj singh <ayushyoraj@gmail.com>
2026-07-31 12:59:00 +0530

    loops

---
+++ 9.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    // for (int i = 1; i <= 100; i++)
+    // {
+    //     cout<<i<<endl;
+    //     i++;
+    // }
+    // int i = 20;
+    // while (i<=60)
+    // {
+    //     cout<<i<<endl;
+    //     i++;
+    // }
+    // int i = 1 ;
+    // do
+    // {
+    //     cout<<i<<endl;
+    //     i++;
+    // } while (i<=50);
+     int i = 1 ;
+    do
+    {
+        cout<<i<<endl;
+        i++;
+    } while (false);
+    
+
+
+    return 0; 
+}
