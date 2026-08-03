940d59e145d088a449417f64cb930123547a8679
Ayush raj singh <ayushyoraj@gmail.com>
2026-08-03 12:11:31 +0530

    new custom code and break and continue

---
+++ 11.cpp
@@ -0,0 +1,27 @@
+//break and continue
+#include<iostream>
+using namespace std;
+
+int main(){
+    // for (int i = 0; i < 7; i++)
+    // {
+    //     /* code */
+    //     // cout<<i<<endl;
+    //     if(i==5){
+    //         break;
+    //     }
+    //     cout<<i<<endl;
+    // }
+    for (int i = 0; i < 67; i++)
+    {
+        /* code */
+        // cout<<i<<endl;
+        if(i==5){
+            continue;
+        }
+        cout<<i<<endl;
+    }
+    
+    ;
+    return 0;
+}
---
+++ shotcode.cpp
@@ -0,0 +1,29 @@
+// {
+	// Place your snippets for cpp here. Each snippet is defined under a snippet name and has a prefix, body and 
+	// description. The prefix is what is used to trigger the snippet and the body will be expanded and inserted. Possible variables are:
+	// $1, $2 for tab stops, $0 for the final cursor position, and ${1:label}, ${2:another} for placeholders. Placeholders with the 
+	// same ids are connected.
+	// Example:
+	// "Print to console": {
+	// 	"prefix": "log",
+	// 	"body": [
+	// 		"console.log('$1');",
+	// 		"$2"
+	// 	],
+	// 	"description": "Log output to console"
+	// }
+	//
+	// You can also restrict snippets to specific files using include/exclude patterns:
+// 	"code": {
+// 		"prefix":  "code code",
+// 		"body":[
+// 		 "#include<iostream>",
+// 		"using namespace std;\n",
+// 		"int main(){",
+// 		"\t$1;",
+// 		"\treturn 0;",
+// 		"}"
+// 	],
+// 	"description": "this is code"
+// 	}
+// }
