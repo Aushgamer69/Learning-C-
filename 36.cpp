3ada64b5d44894e351d986b2fc8cb0991e44cf95
Ayush raj singh <ayushyoraj@gmail.com>
2026-09-08 12:26:45 +0530

    Copy Constructor

---
+++ 36.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+class number
+{
+    int a ;
+    public:
+    number(){
+        a = 0 ;
+    }
+    number(int num){
+        a = num;
+    }
+    number(number &obj){
+        cout<<" copy constructor called "<<endl;
+        a = obj.a;
+    }
+    void display(){
+        cout<<" the number for this object is "<<a<<endl;  
+    }
+};
+int main(){
+  number x ,y(23) ,z(57);
+  x.display();
+  z.display();
+  y.display();
+  number z1(z );
+  z1.display();
+
+    return 0;
+}
